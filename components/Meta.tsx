728715e0476bac689876db07437cebaee73e0c0d
kaka.hu <kaka.huhai@gmail.com>
2024-03-02 11:46:41 +0800

    remove a element

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -5,18 +5,17 @@ export const Meta = ({ title, description, url }) => {
   return (
     <Head>
       <title>{title}</title>
-      <link rel="icon" href="/images/favicon.svg" type="image/png" />
+      <link rel="icon" href="/static/favicon.ico" type="image/png" />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/images/favicon-32x32.png"
+        href="/static/favicon_32.png"
       />
       <meta content={description} name="description" />
       <meta name="og:url" content={url} />
       <meta name="og:title" content={title} />
       <meta name="og:description" content={description} />
-      <meta name="og:image" content={"https://transform.tools/cover.png"} />
       <meta name="og:type" content="website" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
